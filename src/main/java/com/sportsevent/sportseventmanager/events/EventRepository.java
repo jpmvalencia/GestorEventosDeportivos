a9dcaafb721bfeb01dccd879001d76fbdc70b4ac
Juan Pablo Méndez Valencia <juan_mendez23231@elpoli.edu.co>
2025-03-25 23:10:57 -0500

    feat(events): implement pagination for event retrieval
    - Add pagination logic in `EventServlet` to handle `page` and `size` query parameters.
    - Update `EventService` to return paginated results with `totalRecords`.
    - Modify `EventRepository` and `EventDAO` to support pagination in fetching events.
    - Ensure `SuccessResponse` includes pagination data for event retrieval.

--- src/main/java/com/sportsevent/sportseventmanager/events/EventRepository.java
+++ src/main/java/com/sportsevent/sportseventmanager/events/EventRepository.java
@@ -3,11 +3,21 @@ package com.sportsevent.sportseventmanager.events;
 import com.sportsevent.sportseventmanager.events.dao.EventDAO;
 import com.sportsevent.sportseventmanager.events.model.Event;
 
+import java.util.List;
+
 public class EventRepository {
     EventDAO eventDAO;
 
     public EventRepository(EventDAO eventDAO) {
         this.eventDAO = eventDAO;
+    }
+
+    public List<Event> getEvents(int page, int size) {
+        return eventDAO.getEvents(page, size);
+    }
+
+    public long getTotalRecords() {
+        return eventDAO.getTotalRecords();
     }
 
     public boolean existsEventByNameAndSport(String eventName, String sport) {
--- src/main/java/com/sportsevent/sportseventmanager/events/EventService.java
+++ src/main/java/com/sportsevent/sportseventmanager/events/EventService.java
@@ -1,15 +1,33 @@
 package com.sportsevent.sportseventmanager.events;
 
 import com.sportsevent.sportseventmanager.common.exception.DuplicateEventException;
+import com.sportsevent.sportseventmanager.common.pagination.dto.PaginationDTO;
 import com.sportsevent.sportseventmanager.common.response.SuccessResponse;
 import com.sportsevent.sportseventmanager.events.dto.EventDTO;
 import com.sportsevent.sportseventmanager.events.model.Event;
+
+import java.util.List;
 
 public class EventService {
     EventRepository eventRepository;
 
     public EventService(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
+    }
+
+    public SuccessResponse getEvents(PaginationDTO paginationDTO) {
+        int page = paginationDTO.getPage();
+        int size = paginationDTO.getSize();
+
+        List<Event> events = eventRepository.getEvents(page, size);
+        long totalRecords = eventRepository.getTotalRecords();
+
+        return new SuccessResponse(
+                "events retrieved successfully",
+                200,
+                events,
+                totalRecords
+        );
     }
 
     public SuccessResponse addEvent(EventDTO eventDTO) throws DuplicateEventException {
